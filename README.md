f81a30eedd3ae32f38477eb934a51b3a4630d9b6
cuiheran <1043086360@qq.com>
2019-12-01 17:03:43 +0800

    change

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-# django_zhulin
+# 毕业设计Django后台项目
 - 实验室内部项目
-- 请配置.env环境：https://github.com/chr1043086360/python-dotenv
+- 请先配置.env环境：https://github.com/chr1043086360/python-dotenv
+- 在python3.5之后的版本可以通过内置的命令创建虚拟环境
+```bash
+python -m venv .venv
+```
+```bash
+source .venv/bin/activate
+```
+- 通过which查看python是否是虚拟环境的python
+
+```bash
+which django-admin
+```
+- 后面可以加上./避免太多层嵌套
+```bash
+django-admin startproject project ./
+```
+- 导入导出需求包
+```bash
+pip freeze > requirements.txt
+```
+```bash
+pip install -r requirements.txt
+```
+
 
--- zhulin/models.py
+++ zhulin/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    SEX = (
+        ('男性', '男性'),
+        ('女性', '女性'),
+    )
+    nickName = models.CharField(max_length=32, unique=True)
+    phoneNum = models.CharField(max_length=16, unique=True)
+    sex = models.CharField(max_length=8, choices=SEX)
+    birth_year = models.IntegerField(default=2000, verbose_name="出生年")
+    birth_month = models.IntegerField(default=1, verbose_name="出生月")
+    birth_day = models.IntegerField(default=1, verbose_name="出生日")
+    avatar = models.CharField(max_length=256, verbose_name="个人形象")
+    location = models.CharField(max_length=32, verbose_name="长居地")
+
+
+class Profile(models.Model):
+    SEX = (
+        ('男性', '男性'),
+        ('女性', '女性'),
+    )
+    location = models.CharField(max_length=32, verbose_name="目标城市")
+
+    min_distance = models.IntegerField(default=1, verbose_name="最小查找范围")
+    max_distance = models.IntegerField(default=10, verbose_name="最大查找范围")
+
+    min_dating_age = models.IntegerField(default=18, verbose_name="最小交友年龄")
+    max_dating_age = models.IntegerField(default=50, verbose_name="最大交友年龄")
+
+    dating_sex = models.CharField(max_length=8,  choices=SEX, verbose_name="匹配性别")
+    vibration = models.BooleanField(default=True, verbose_name="是否要开启震动")
+    only_match = models.BooleanField(default=True, verbose_name="不让为匹配的人看我的相册")
+    auto_play = models.BooleanField(default=True, verbose_name="是否自动播放视频")
